01651a1872130eb547f533ce4a3049e81434cd0e
ouioui99 <qp5minute@gmail.com>
2024-07-15 21:54:51 +0900

    未ログイン時にhooterを非表示

--- react-vite-calender/src/router/Router.tsx
+++ react-vite-calender/src/router/Router.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -8,8 +8,11 @@ import Setting from "../pages/Setting";
 import BottomNavigation from "../components/BottomNavigations/BottomNavigation";
 import PublicRouter from "./PublicRouter";
 import PrivateRouter from "./PrivateRouter";
+import { UserAuthContext } from "../Contexts/UserContext";
 
 export default function Router() {
+  const { user, setUser, loading, setLoading }: any =
+    useContext(UserAuthContext);
   return (
     <div className="flex flex-col h-screen">
       <BrowserRouter>
@@ -18,19 +21,17 @@ export default function Router() {
           <Route element={<PublicRouter />}>
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
+            <Route path="*" element={<h1>Not Found Page</h1>} />
           </Route>
           {/* private routes */}
           <Route element={<PrivateRouter />}>
             <Route path="/" element={<Home />} />
+            <Route path="/regist-shift" element={<RegistShift />} />
+            <Route path="/setting" element={<Setting />} />
+            <Route path="*" element={<h1>Not Found Page</h1>} />
           </Route>
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/regist-shift" element={<RegistShift />} />
-          <Route path="/setting" element={<Setting />} />
-          <Route path="*" element={<h1>Not Found Page</h1>} /> */}
         </Routes>
-        <BottomNavigation />
+        {user ? <BottomNavigation /> : <></>}
       </BrowserRouter>
     </div>
   );
